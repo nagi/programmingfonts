8c85685f8f046c390cda093185a367018772f3a1
Andrew Nagi <andrew.nagi@gmail.com>
2012-02-01 18:42:45 +0000

    Generate font related css using database.

--- app/models/font.rb
+++ app/models/font.rb
@@ -1,2 +1,12 @@
 class Font < ActiveRecord::Base
+  def unix_name
+    name.parameterize
+  end
+
+  def font_type
+    case Pathname.new(filename).extname
+    when '.ttf' then 'truetype'
+    when '.otf' then 'opentype'
+    end
+  end
 end
--- spec/models/font_spec.rb
+++ spec/models/font_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Font do
+  describe "#unix_name" do
+    it "should not include non word characters (apart from dashes)" do
+      Font.new(name:'ariel BOLD_!"$%^&*()-=').unix_name.delete('-').should_not match(/\W+/)
+    end
+  end
+
+  describe "#font_type" do
+    it "identifies *.ttf fonts as truetype" do
+      Font.new(filename:'ariel.ttf').font_type.should eql('truetype')
+    end
+
+    it "identifies *.otf fonts as opentype" do
+      Font.new(filename:'ariel.otf').font_type.should eql('opentype')
+    end
+  end
 end
