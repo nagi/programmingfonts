51659c86766957652ecf40cd66a96b00772181d6
Andrew Nagi <andrew.nagi@gmail.com>
2012-02-03 09:48:07 +0000

    Add spec support file.

---
+++ spec/support/blueprints.rb
@@ -0,0 +1,8 @@
+require 'machinist/active_record'
+
+Font.blueprint do
+  name { "Inconsolata" }
+  filename { "Inconsolata.otf" }
+  description { "Inconsolata is a monospace font, designed for code listings and the like." }
+  link { "http://levien.com/type/myfonts/inconsolata.html" }
+end
