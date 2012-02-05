46dd5d6ed1bd630f8af143e7e6bd1564c483f90d
Andrew Nagi <andrew.nagi@gmail.com>
2012-02-05 19:12:41 +0000

    Require highlighter brush files in manifest.
    Autoloader feature of syntax highlighter has known bugs, so I'm just
    going to compress and serve these via the pipeline.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,17 @@
 //
 //= require shCore
 //
+//= require shBrushRuby
+//= require shBrushJScript
+//= require shBrushJava
+//= require shBrushCpp
+//= require shBrushPhp
+//= require shBrushPython
+//= require shBrushCSharp
+// dont require shBrushPerl : Perl brush condenses code for some reason.
+//= require shBrushSql
+//= require shBrushBash
+//= require shBrushVb
+//= require shBrushObjC
+//
 //= require fonts
